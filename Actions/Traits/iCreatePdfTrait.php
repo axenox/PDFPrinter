977153f530a8fbcac1914bc90c2b07627aad2a7d
hail-cookies <81558994+hail-cookies@users.noreply.github.com>
2026-03-26 14:01:52 +0100

    DEV iCreatePdfTrait added option to show fixed elements on first page

--- Actions/Traits/iCreatePdfTrait.php
+++ Actions/Traits/iCreatePdfTrait.php
@@ -8,7 +8,8 @@ use axenox\PDFPrinter\Interfaces\Actions\iCreatePdf;
 trait iCreatePdfTrait
 {
     private $orientation = 'landscape';
-    
+    private bool $hideFixedElementsOnFirstPage = true;
+
     /**
      * @uxon-property orientation
      * @uxon-type [portrait,landscape]
@@ -22,12 +23,38 @@ trait iCreatePdfTrait
         $this->orientation = $value;
         return $this;
     }
-    
+
     public function getOrientation() : string
     {
         return $this->orientation;
     }
-    
+
+    /**
+     * Configure, whether elements with the style option `"position": "fixed"` should be hidden
+     * on the first page of the PDF. 
+     * 
+     * This setting is TRUE by default, to prevent footers and headers from being rendered on the first page.
+     * 
+     * @uxon-property hide_fixed_elements_on_first_page
+     * @uxon-type boolean
+     * 
+     * @param bool $value
+     * @return iCreatePdf
+     */
+    public function setHideFixedElementsOnFirstPage(bool $value): iCreatePdf
+    {
+        $this->hideFixedElementsOnFirstPage = $value;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getHideFixedElementsOnFirstPage(): bool
+    {
+        return $this->hideFixedElementsOnFirstPage;
+    }
+
     /**
      * Returns dompdf->output() stream to save in a file.
      *
@@ -39,12 +66,13 @@ trait iCreatePdfTrait
         // instantiate and use the dompdf class
         $dompdf = new Dompdf();
         
-        // Add callbacks to customize rendering.
-        $dompdf->setCallbacks([
+        $callbacks = [];
+        
+        if($this->getHideFixedElementsOnFirstPage()) {
             // Skip rendering frames with "fixed" positioning on the first page. This mostly results in automatic
             // headers and footers only being rendered on the second page and beyond.
             // TODO geb 2026-03-24: We might need a more control over what pages fixed frames will be rendered on. 
-            [
+            $callbacks[] = [
                 'event' => 'begin_frame',
                 'f' => function ($frame, $canvas, $fontMetrics) {
                     $style = $frame->get_style();
@@ -52,8 +80,13 @@ trait iCreatePdfTrait
                         $style->set_used("display", "none");
                     }
                 }
-            ]
-        ]);
+            ];
+        }
+        
+        if(!empty($callbacks)) {
+            // Add callbacks to customize rendering.
+            $dompdf->setCallbacks($callbacks);
+        }
         
         $options = $dompdf->getOptions();
         $options->setIsRemoteEnabled(true);
@@ -67,5 +100,5 @@ trait iCreatePdfTrait
         $dompdf->render();
         return $dompdf->output();
     }
-    
+
 }
